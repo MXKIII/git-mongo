611c964ac8a31adb510fd9bd73fc3a3cb6fb03d1
Pret Secretariat <pretsecretariat@administration.paris-sorbonne.fr>
2025-03-07 11:49:01 +0100

    commit

--- controller/recipesController.js
+++ controller/recipesController.js
@@ -13,6 +13,21 @@ export const getAllRecipes = async (req,res)=>{
     }
 }
 
+export const getRecipeById = async (req,res)=>{
+    const {id} = req.params
+    try {
+       const recipeById = await recipes.findOne({author:id})
+       if(!recipeById){
+        return res.status(404).json({ message: 'No recipe found' });
+       }
+       return res.status(200).json(recipeById)
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({message:"internal server error"})
+    }
+}
+
+
 export const createRecipe = async (req, res)=>{
     try {
         const newRecipe= await recipes.create(req.body)
@@ -23,3 +38,35 @@ export const createRecipe = async (req, res)=>{
     }
 }
 
+export const updateRecipe = async (req,res)=>{
+    const {id} = req.params
+    try {
+        const recipeById = await recipes.findOneAndUpdate({ author: id },req.body,{ new: true });
+       if(!recipeById){
+        return res.status(404).json({ message: 'No recipes found' });
+       }
+       else{
+        recipeById.save()
+        return res.json({ message: "recipe has been updated", recipe: recipeById});
+       }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({message:"internal server error"});
+    }
+}
+
+export const deleteRecipe = async (req,res)=>{
+    const {id} = req.params
+    try {
+       const deletedRecipe = await recipes.findOneAndDelete({author: id})
+       if(!deletedRecipe){
+        return res.status(404).json({ message: 'No recipe found' });
+       }
+       else{
+        return res.json({message:"recipe has been deleted"});
+       }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({message:"internal server error"});
+    }
+}
--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createRecipe, getAllRecipes } from "../controller/recipesController.js";
+import { createRecipe, deleteRecipe, getAllRecipes, getRecipeById, updateRecipe } from "../controller/recipesController.js";
 
 
 const recipesRouter = Router()
 recipesRouter.get('/recipes', getAllRecipes)
+recipesRouter.get('/recipes/:id', getRecipeById)
 recipesRouter.post('/recipes', createRecipe)
+recipesRouter.put('/recipes/:id', updateRecipe)
+recipesRouter.delete('/recipes/:id',deleteRecipe)
 export default recipesRouter
